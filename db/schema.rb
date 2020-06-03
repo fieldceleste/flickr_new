221af15d478067c5526861ed02544cddccb8a183
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-06-03 09:53:37 -0700

    add profile id to photos table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_163637) do
+ActiveRecord::Schema.define(version: 2020_06_03_165126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_163637) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "profile_id"
   end
 
   create_table "profiles", force: :cascade do |t|
